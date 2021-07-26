bb0c3f722792eb2faf1149dbddd7612b53476d56
Lukas Klingsbo <me@lukas.fyi>
2021-07-26 14:44:38 +0200

    Add descend speed

--- lib/player.dart
+++ lib/player.dart
@@ -1,6 +1,10 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flutter/animation.dart';
 
 class PlayerComponent extends SpriteComponent {
+  static const double downSpeed = 200.0;
+
   PlayerComponent({
     required Vector2 position,
     required Vector2 size,
@@ -11,10 +15,30 @@ class PlayerComponent extends SpriteComponent {
     anchor = Anchor.center;
   }
 
+  MoveEffect? _currentEffect;
+
   /// This is called when the game is tapped.
   /// You could either use a [MoveEffect] here to create some movement, or just
   /// modify [position] directly.
   void bounce() {
-    // TODO: React to input
+    if (_currentEffect != null) {
+      removeEffect(_currentEffect!);
+    }
+    _currentEffect = MoveEffect(
+      path: [Vector2(0, -60.0)],
+      speed: 300,
+      curve: Curves.ease,
+      isRelative: true,
+      isAlternating: true,
+    );
+    addEffect(_currentEffect!);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (effects.isEmpty) {
+      position.y += dt * downSpeed;
+    }
   }
 }
